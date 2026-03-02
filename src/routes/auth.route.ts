8b8a55b41d95bffb5d4fdde016f6bdb535d53711
unknown <iamadelowotan@gmail.com>
2026-03-02 22:45:09 +0100

    update: removed redundanct code

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { authController } from "../controllers/auth.controller.js";
-// import { throttleAction, validateUser } from "../services/middleware.js";
 
 const authRoute = Router();
 
-authRoute.post(
-  "/login",
-  // throttleAction({ action: "login", limit: 5, windowSeconds: 300 }),
-  authController.login,
-);
+authRoute.post("/login", authController.login);
 authRoute.post(
   "/register",
   // throttleAction({ action: "register", limit: 5, windowSeconds: 300 }),
